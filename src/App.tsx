67363969671aeabd5561870ea577a40b9eee83ed
Bence A. Tóth <tothab@gmail.com>
2022-12-16 14:58:28 +0100

    Created primary site elements

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,24 @@
-const App = () => <div></div>;
+import Logo from "./Logo";
+
+import "./App.css";
+
+const App = () => (
+  <div className="site">
+    <header>
+      <div className="site-header-inner-wrapper">
+        <div className="logo-wrapper">
+          <Logo />
+          Rest of the header will come here
+        </div>
+      </div>
+    </header>
+    <main>
+      <div className="site-main-inner-wrapper">Main content will come here</div>
+    </main>
+    <footer>
+      <div className="site-footer-inner-wrapper">Footer will come here</div>
+    </footer>
+  </div>
+);
 
 export default App;
