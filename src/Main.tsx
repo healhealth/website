533931a3894e6fc91cad0eda3c618cc9e5063d72
Bence A. Tóth <tothab@gmail.com>
2023-01-12 18:44:29 +0100

    Added some content

--- src/Main.tsx
+++ src/Main.tsx
@@ -3,42 +3,87 @@ import "./Main.css";
 const Main = () => (
   <main className="site-main">
     <div className="site-main-inner-wrapper">
-      <h2>About us</h2>
-      <p>
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consectetur
-        eum quo, quasi rem ut sit quibusdam error neque perferendis doloremque
-        dolores impedit expedita vero similique ad? Nostrum labore blanditiis
-        molestiae!
-      </p>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam error eum
-        mollitia fugiat quia ab libero animi nisi asperiores consequuntur vero
-        qui neque labore inventore nihil quisquam tenetur, debitis maxime?
-      </p>
-      <h2>Products and solutions</h2>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae officia id
-        amet sint et repellat corporis sapiente maiores, voluptatum modi
-        suscipit ab aut in ex, repellendus doloribus eius reiciendis. Sed.
-      </p>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eaque, dicta
-        modi. Molestiae dolor fugiat libero adipisci sit quis. Minus blanditiis
-        veniam quibusdam aliquam! Ullam inventore itaque sed excepturi ad unde.
-      </p>
-      <h2>Our amazing people</h2>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum, excepturi.
-        Debitis dolorem obcaecati eaque ullam repellat porro. Non accusantium
-        nesciunt perferendis repudiandae magnam nostrum iure, distinctio
-        doloribus inventore voluptates molestiae.
-      </p>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt
-        blanditiis nemo tenetur unde rerum necessitatibus, mollitia odio vel
-        totam recusandae maxime optio reiciendis incidunt distinctio sit aliquid
-        neque velit possimus?
-      </p>
+      <section>
+        <h2>Our vision and mission</h2>
+        <p>Mental wellbeing is for all of us.</p>
+        <p>
+          Our mission is to utilize technology to improve mental health and
+          wellbeing for everyone. We empower mental health professionals with
+          innovative tools and resources, while making self-help accessible to
+          all through our free and open source digital products.
+        </p>
+      </section>
+      <section>
+        <h2>Our services</h2>
+        <h3>Self-help tools</h3>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur
+          voluptas exercitationem quod ad architecto, qui, reiciendis
+          necessitatibus odio nemo tempora sequi quia blanditiis, harum modi
+          beatae? Quam hic ullam aut.
+        </p>
+        <h3>Tools for mental health professionals</h3>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur
+          voluptas exercitationem quod ad architecto, qui, reiciendis
+          necessitatibus odio nemo tempora sequi quia blanditiis, harum modi
+          beatae? Quam hic ullam aut.
+        </p>
+        <h3>Consultancy</h3>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur
+          voluptas exercitationem quod ad architecto, qui, reiciendis
+          necessitatibus odio nemo tempora sequi quia blanditiis, harum modi
+          beatae? Quam hic ullam aut.
+        </p>
+      </section>
+      <section>
+        <h2>Our team</h2>
+        <div className="our-team">
+          <div className="team-member">
+            <img src="https://placekitten.com/g/320/320" alt="" />
+            <div>
+              <div className="name">Nashmil Sepehriazad</div>
+              <div className="role">Founder, Managing Director</div>
+            </div>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Aspernatur enim nulla culpa quam ipsam. Iure, architecto culpa
+              exercitationem dolores.
+            </p>
+          </div>
+          <div className="team-member">
+            <img src="https://placekitten.com/g/320/320" alt="" />
+            <div>
+              <div className="name">Bence A. Tóth</div>
+              <div className="role">Principal Software Engineer</div>
+            </div>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Aspernatur enim nulla culpa quam ipsam. Iure, architecto culpa
+              exercitationem dolores.
+            </p>
+          </div>
+          <div className="team-member">
+            <img src="https://placekitten.com/g/320/320" alt="" />
+            <div>
+              <div className="name">Marcell Ákos Tóth</div>
+              <div className="role">Graphics Designer</div>
+            </div>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Aspernatur enim nulla culpa quam ipsam. Iure, architecto culpa
+              exercitationem dolores.
+            </p>
+          </div>
+        </div>
+      </section>
+      <section>
+        <h2>Support us</h2>
+      </section>
+      <section>
+        <h2>Join us</h2>
+      </section>
     </div>
   </main>
 );
