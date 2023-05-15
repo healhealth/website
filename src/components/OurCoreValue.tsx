2d4dde8effcebf29817923e24dd1367f29d04080
DEJENERUTA <dejenedaba269@gmail.com>
2023-05-15 20:50:24 +0200

    formate our core value component

--- src/components/OurCoreValue.tsx
+++ src/components/OurCoreValue.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import "./OurCoreValue.css";
 
 const OurCoreValue = () => (
@@ -16,15 +17,15 @@ const OurCoreValue = () => (
         of dedicated individuals, but we are looking to grow and expand our
         capabilities. We are looking for volunteers and interns who are eager to
         learn and contribute their skills in areas such as product development,
-        marketing, research, sales, finances and more.
+        marketing, research, sales, finances, and more.
       </p>
       <p>
         We value diversity and welcome applicants with a wide range of
         backgrounds and experiences. If you're interested in joining our team
-        and making a meaningful impact in the field of mental health, please get
-        in touch with us at the email address: nashmil@healhealth.dk. We look
-        forward to hearing from you and working together to make mental health
-        care accessible to all.
+        and making a meaningful impact in the field of mental health, please
+        get in touch with us at the email address: nashmil@healhealth.dk. We
+        look forward to hearing from you and working together to make mental
+        health care accessible to all.
       </p>
     </section>
   </div>
