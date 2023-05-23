b3662a1070bf30a4f4988f7b7a857aea6a8737a0
Dejene Daba <dejenedaba269@gmail.com>
2023-05-23 22:22:47 +0200

    add our core value text

--- src/components/OurCoreValue.tsx
+++ src/components/OurCoreValue.tsx
@@ -3,27 +3,30 @@ const OurCoreValue = () => (
     <section>
       <h2>Our Core Value</h2>
       <p>
-        Are you passionate about mental health and technology? Are you looking
-        for an opportunity to gain experience and make a difference in the
-        field? We are excited to announce that Heal Health is currently seeking
-        new members to join our team on a voluntary or unpaid internship basis.
+      The core values are the beliefs, philosophies, and principles which reflect the company culture and impact the employee's experience, as well as the relationship with customers, partners, and shareholders.
       </p>
       <p>
-        As a startup, we are dedicated to creating innovative and effective
-        mental health technology to support those in need. We are a small team
-        of dedicated individuals, but we are looking to grow and expand our
-        capabilities. We are looking for volunteers and interns who are eager to
-        learn and contribute their skills in areas such as product development,
-        marketing, research, sales, finances, and more.
+      <h3>Promoting Mental Wellbeing</h3>
+         Many of our important values fall under this crucial category, reflecting our mission, by promoting:
+<ul>
+  <li>Opportunity For Growth (Learning and Accomplishment)</li>
+  <li>Mattering At Work (Dignity and Meaning)</li> 
+  <li>Work-Life Harmony (Autonomy and Flexibility)</li> 
+  <li>Connection and Community (Belonging and Social Support)</li>
+</ul>
       </p>
       <p>
-        We value diversity and welcome applicants with a wide range of
-        backgrounds and experiences. If you're interested in joining our team
-        and making a meaningful impact in the field of mental health, please get
-        in touch. We look
-        forward to hearing from you and working together to make mental health
-        care accessible to all.
+      <h3>utual Respect</h3>
+         Mutual respect is the bedrock of any healthy relationship and serves as a fundamental pillar of humane and ethical behavior. It entails deeply valuing an individual's right to exist, be heard, and enjoy equal opportunities as others. Respect extends beyond overlooking or merely tolerating differences; instead, it encompasses the recognition of these differences, understanding their significance, and responding with genuine interest, politeness, and care.
+
       </p>
+      <p>
+      <h3>Social Justice</h3>
+       We believe in fairness and equality in the distribution of mental health resources and services. This value is rooted in the idea that everyone should have access to the care they need regardless of their financial status or other factors.
+      </p>
+      <p>
+        <h3>Diversity</h3>
+          We are committed to upholding diversity and inclusiveness in our hiring and promotion processes, ensuring that every individual has equal access to opportunities and advancement within our company.</p>
      
     </section>
   </div>
