9e2134858e37c41b7f8aef6449f34f4b532cbd69
Dejene Daba <dejenedaba269@gmail.com>
2023-06-06 08:48:36 +0200

    Add space between 'at' and email address in Join component (line 24)

--- src/components/Join.tsx
+++ src/components/Join.tsx
@@ -21,8 +21,8 @@ const Join = () => (
         We value diversity and welcome applicants with a wide range of
         backgrounds and experiences. If you’re interested in joining our team
         and making a meaningful impact in the field of mental health, please get
-        in touch with us at
-        <a href="mailto:info@healhealth.dk">info@healhealth.dk</a>. We look
+        in touch with us at:&nbsp;
+        <a href="mailto:info@healhealth.dk"> info@healhealth.dk</a>. We look
         forward to hearing from you and working together to make mental health
         care accessible to all.
       </p>
